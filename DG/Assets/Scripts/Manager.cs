00c1a2c0df22232142eea59e9895364ede48bea9
Adrián García <adriangarciagon@hotmail.com>
2015-10-08 03:50:22 +0200

    Segundo Commit de pruebas con resultados satisfactorios.

--- DG/Assets/Scripts/Manager.cs
+++ DG/Assets/Scripts/Manager.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class Manager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void Pruebas() {
 
     }
